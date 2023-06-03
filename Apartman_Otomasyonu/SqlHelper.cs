e7f0d40c9f28ed1e47ac420649364197c1cc9fe0
Mferik <mfatiherikk@gmail.com>
2023-06-03 16:18:48 +0300

    SqlHelper class added

---
+++ Apartman_Otomasyonu/SqlHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apartman_Otomasyonu
+{
+    
+
+    internal class SqlHelper
+    {
+        private string ConnectionString { get; set; }
+        public SqlConnection Connection { get; set; }
+
+        public SqlHelper()
+        {
+            Connection = new SqlConnection();
+            ConnectionString = @"Data Source =MFE\SQLEXPRESS;initial Catalog =APARTMAN;Integrated Security=True";
+        }
+
+        public void ExecuteProc(string procName, params SqlParameter[] ps)
+        {
+            SqlCommand command = new SqlCommand();
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            command.CommandText = procName;
+            command.Parameters.AddRange(ps);
+            command.Connection = Connection;
+            Connection.Open();
+            command.ExecuteNonQuery();
+            Connection.Close();
+        }
+
+    }
+}
